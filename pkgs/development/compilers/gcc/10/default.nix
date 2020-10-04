253f5f809b319f7cd76719d758ed52c4f24634ef
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-10-04 14:57:00 +0200

    gcc 9, 10: add warn for bug 95189 patch
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- pkgs/development/compilers/gcc/10/default.nix
+++ pkgs/development/compilers/gcc/10/default.nix
@@ -51,8 +51,8 @@ let majorVersion = "10";
 
     inherit (stdenv) buildPlatform hostPlatform targetPlatform;
 
-    patches =
-         optional (targetPlatform != hostPlatform) ../libstdc++-target.patch
+    patches = [ ../warn-for-bug-95189.patch ]
+      ++ optional (targetPlatform != hostPlatform) ../libstdc++-target.patch
       ++ optional noSysDirs ../no-sys-dirs.patch
       /* ++ optional (hostPlatform != buildPlatform) (fetchpatch { # XXX: Refine when this should be applied
         url = "https://git.busybox.net/buildroot/plain/package/gcc/${version}/0900-remove-selftests.patch?id=11271540bfe6adafbc133caf6b5b902a816f5f02";
